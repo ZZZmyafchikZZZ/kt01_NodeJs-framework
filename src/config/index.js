4caa68accf0c0195676d92f9b94eab0e7e4c22c9
ZZZmyafchikZZZ <imanovv2008@gmail.com>
2026-09-21 19:26:51 +0300

    Delete src directory

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  inputFile: 'data/users.csv',
-  outputFile: 'data/result.csv',
-  minAge: 18,
-  cityFilter: 'Москва'
-};
--- src/index.js
+++
@@ -1,8 +0,0 @@
-const { processCsv } = require('./utils/csv');
-const configData = require('./config/index');
-
-// Делаем конфиг доступным для функции
-global.config = configData;
-
-// Запуск
-processCsv();
